bd332d7eced0f185e6dd5ba741825acb13fc0d19
Tadeusz Łazurski <tadeusz@lazurski.pl>
2016-08-06 17:15:06 +0200

    Elm app with awesome list of cats
    My first Elm app 🎉

---
+++ cats-list.elm
@@ -0,0 +1,120 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as App
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (..)
+
+
+main =
+    App.beginnerProgram
+        { model = initial
+        , view = view
+        , update = logger update
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { cats : List Cat
+    , input : Cat
+    }
+
+
+type alias Cat =
+    String
+
+
+initial : Model
+initial =
+    { cats =
+        [ "Katiusza"
+        , "Skubi"
+        , "George"
+        ]
+    , input = ""
+    }
+
+
+
+-- UPDATE
+
+
+type Action
+    = Remove Cat
+    | Select Cat
+    | Add
+    | Input Cat
+    | Restore
+
+
+logger update =
+    \action model ->
+        model |> (Debug.log "action" action |> update) |> Debug.log "model"
+
+
+update action model =
+    case action of
+        Input cat ->
+            { model | input = cat }
+
+        Add ->
+            if model.input == "" then
+                model
+            else
+                { model
+                    | cats = model.input :: model.cats
+                    , input = ""
+                }
+
+        Remove cat ->
+            let
+                predicate item =
+                    item /= cat
+            in
+                { model
+                    | cats = List.filter predicate model.cats
+                    , input = cat
+                }
+
+        Select cat ->
+            let
+                predicate item =
+                    item == cat
+            in
+                { model | cats = List.filter predicate model.cats }
+
+        Restore ->
+            { model | cats = initial.cats }
+
+
+
+-- VIEW
+
+
+view model =
+    div []
+        [ input [ onInput Input, value model.input ] []
+        , button [ onClick Add ]
+            [ text "New cat!"
+            ]
+        , ul []
+            (List.map item model.cats)
+        , button [ onClick Restore ]
+            [ text "Restore kittens!"
+            ]
+        ]
+
+
+item cat =
+    li []
+        [ a [ onClick (Select cat), href "#" ]
+            [ text cat
+            ]
+        , button [ onClick (Remove cat) ]
+            [ text "x"
+            ]
+        ]
